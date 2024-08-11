9bbb8d22b22efaf43bdcbb214e99f1af32ad196b
ahmedehab0 <125496251+ahmedehab0@users.noreply.github.com>
2024-08-11 20:33:11 +0300

    initiate the express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import { config } from 'dotenv'
+import routes from './server/Routes/index.js'
+
+config()
+
+const app = express()
+
+app.use(express.json())
+app.use('api', routes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`)
+})
---
+++ server/Routes/generateQsRoute.js
@@ -0,0 +1,1 @@
+import generateQs from "../../tools/generateQs";
