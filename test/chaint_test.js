6589d8942539a61632e48f932ac7d29cdedc1b47
oovaa <omarvx211@gmail.com>
2024-08-13 09:10:41 +0200

    tested the tools and everything is good 👍 at 13-09

--- test/chaint_test.js
+++ test/chaint_test.js
@@ -2,13 +2,23 @@ import { getSummarizeRes } from '../tools/summarize.js'
 import generateQs from '../tools/generateQs.js'
 import { parser } from '../utils/parser.js'
 
-
 // For testing
 async function testSummarization() {
+  const info = await parser('./story.txt')
+  const data = await getSummarizeRes(info)
+  console.log(data)
+}
+
+async function testQA() {
   const info = await parser('./story.txt')
   const data = await generateQs(info)
   console.log(data)
 }
+//   // Uncomment to run the test
+async function runTests() {
+  await testSummarization()
+  console.log('=================================')
+  await testQA()
+}
 
-//   / Uncomment to run the test
-testSummarization()
+runTests()
