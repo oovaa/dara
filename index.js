68c65355351a2165d9308c5ac382e3c9b33241e6
Shakir Abdo <shicolare1@gmail.com>
2024-08-20 15:10:36 +0300

    add root endpoint

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-import express from 'express'
 import { config } from 'dotenv'
+config()
+
+import express from 'express'
+import {engine} from 'express-handlebars'
+import compression from 'compression'
 import routes from './server/routes/index.js'
 import globalErrorHandler from './server/middlewares/globalErrorHandler.js'
 import morgan from 'morgan'
 
-config()
 
 const app = express()
-
+app.use(compression())
 app.use(express.json())
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 app.use('/api', routes)
 app.use(globalErrorHandler)
 app.use(morgan('dev'))
-
+app.get('/', (req, res) => {
+  res.render('home', {DARAFRONT: process.env.FRONT_DOMAIN})
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, async () => {
