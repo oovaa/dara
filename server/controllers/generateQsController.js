a59453cbb47331cdc90c1e69c09ea2981760c1f0
Shakir Abdo <shicolare1@gmail.com>
2024-08-24 00:03:12 +0300

    process files on the fly for generateQs

--- server/controllers/generateQsController.js
+++ server/controllers/generateQsController.js
@@ -1,5 +1,6 @@
 import generateQs from "../../tools/generateQs.js";
 import { parser } from "../../utils/parser.js";
+import tempWrite from 'temp-write'
 import expressAsyncHandler from "express-async-handler";
 import ApiError from "../utils/ApiError.js";
 
@@ -10,10 +11,9 @@ const generate = expressAsyncHandler(async (req, res, next) => {
         )
     }
     try {
-       const docs = await parser(req.file.path)
+        const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
+       const docs = await parser(filePath)
        const Qs = await generateQs(docs)
-       console.log(Qs);
-
        return res.status(200).json(JSON.parse(Qs, 2, null))
     } catch (err) {
        return next (
