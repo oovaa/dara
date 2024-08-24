0359e848152a72d3f23e3e9c6fb07303ee12a753
Shakir Abdo <shicolare1@gmail.com>
2024-08-25 00:47:41 +0300

    add chat tool

--- server/controllers/generateQsController.js
+++ server/controllers/generateQsController.js
@@ -1,25 +1,21 @@
-import generateQs from "../../tools/generateQs.js";
-import { parser } from "../../utils/parser.js";
+import generateQs from '../../tools/generateQs.js'
+import {parser} from '../../utils/parser.js'
 import tempWrite from 'temp-write'
-import expressAsyncHandler from "express-async-handler";
-import ApiError from "../utils/ApiError.js";
+import expressAsyncHandler from 'express-async-handler'
+import ApiError from '../utils/ApiError.js'
 
 const generate = expressAsyncHandler(async (req, res, next) => {
-    if (!req.file) {
-        next(
-            new ApiError("you must provide a file", 400)
-        )
-    }
-    try {
-        const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
-        const docs = await parser(filePath)
-        const Qs = await generateQs(docs)
-       return res.status(200).json(JSON.parse(Qs, 2, null))
-    } catch (err) {
-       return next (
-        new ApiError(err.message, 500)
-       )
-    }
+  if (!req.file) {
+    next(new ApiError('you must provide a file', 400))
+  }
+  try {
+    const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
+    const docs = await parser(filePath)
+    const Qs = await generateQs(docs)
+    return res.status(200).json(JSON.parse(Qs, 2, null))
+  } catch (err) {
+    return next(new ApiError(err.message, 500))
+  }
 })
 
 export default generate
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import Router from 'express'
 import QsRoutes from './generateQsRoute.js'
 import SumRoutes from './generateSummaryRoute.js'
+import chat from './answerQsRoute.js'
 
 const router = Router()
 
 router.use('/qs', QsRoutes)
 router.use('/sum', SumRoutes)
+router.use('/chat', chat)
 
 export default router
