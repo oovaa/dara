943ca84de9e7cead151ff47bfdb5f57adf181422
ahmedehab0 <125496251+ahmedehab0@users.noreply.github.com>
2024-08-25 17:30:45 +0300

    fixed the json format error in the generate qs controller and modified the parser to handel the errors using try&catch

--- server/controllers/generateQsController.js
+++ server/controllers/generateQsController.js
@@ -1,21 +1,27 @@
-import generateQs from '../../tools/generateQs.js'
-import {parser} from '../../utils/parser.js'
-import tempWrite from 'temp-write'
-import expressAsyncHandler from 'express-async-handler'
-import ApiError from '../utils/ApiError.js'
+import generateQs from "../../tools/generateQs.js";
+import { parser } from "../../utils/parser.js";
+import tempWrite from "temp-write";
+import expressAsyncHandler from "express-async-handler";
+import ApiError from "../utils/ApiError.js";
 
 const generate = expressAsyncHandler(async (req, res, next) => {
   if (!req.file) {
-    next(new ApiError('you must provide a file', 400))
+    next(new ApiError("you must provide a file", 400));
   }
   try {
-    const filePath = tempWrite.sync(req.file.buffer, req.file.originalname)
-    const docs = await parser(filePath)
-    const Qs = await generateQs(docs)
-    return res.status(200).json(JSON.parse(Qs, 2, null))
-  } catch (err) {
-    return next(new ApiError(err.message, 500))
-  }
-})
+    const filePath = tempWrite.sync(req.file.buffer, req.file.originalname);
+    const docs = await parser(filePath);
+    if (!docs) {
+      return next(new ApiError("no docs", 404));
+    }
 
-export default generate
+    const Qs = await generateQs(docs);
+    const qs_json = JSON.parse(JSON.stringify(Qs));
+
+    return res.status(200).json(qs_json);
+  } catch (err) {
+    return next(new ApiError(err.message, 500));
+  }
+});
+
+export default generate;
--- utils/parser.js
+++ utils/parser.js
@@ -1,59 +1,62 @@
 import { TextLoader } from "langchain/document_loaders/fs/text";
-import {PDFLoader} from '@langchain/community/document_loaders/fs/pdf'
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
 import { PPTXLoader } from "@langchain/community/document_loaders/fs/pptx";
-import pdf from 'pdf-parse-debugging-disabled';
-import fs from 'fs';
-import path from 'path';
+import path from "path";
 import { config } from "dotenv";
 config();
 
-
-const parser = async filePath => {
+const parser = async (filePath) => {
   const ext = path.extname(filePath).toLowerCase();
-  let loader = '';
-  let docs = '';
-  let allSlidesContent = '';
+  let loader = "";
+  let docs = "";
+  let allSlidesContent = "";
 
   switch (ext) {
-    case ('.pptx'):
-     loader = PPTXLoader(filePath);
-     docs = await loader.load();
-     allSlidesContent = docs.map(doc => doc.pageContent);
-    
-     return allSlidesContent;
-    
-    case '.pdf':
-      // const pdfBuffer = fs.readFileSync(filePath);
-      // try {
-      //   const data = await pdf(pdfBuffer);
-      //   return data.text;
-      // } catch (error) {
-      //   console.error("Error parsing PDF:", error);
-      //   return null;
-      // }
-      loader = new PDFLoader(filePath);
-      docs = await loader.load();
-      allSlidesContent = docs.map(doc => doc.pageContent);
-    
-      return allSlidesContent;
-    
-    case '.docx':
-      loader = new DocxLoader(filePath);
-      docs = await loader.load();
-      allSlidesContent = docs.map(doc => doc.pageContent);
-    
-      return allSlidesContent;
-    
-    case '.txt':
-      loader = new TextLoader(filePath)
-      docs = await loader.load();
-      allSlidesContent = docs.map(doc => doc.pageContent);
-    
-      return allSlidesContent;
+    case ".pptx":
+      try {
+        loader = new PPTXLoader(filePath);
+        docs = await loader.load();
+        allSlidesContent = docs.map((doc) => doc.pageContent);
+
+        return allSlidesContent;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+
+    case ".pdf":
+      try {
+        loader = new PDFLoader(filePath);
+        docs = await loader.load();
+        allSlidesContent = docs.map((doc) => doc.pageContent);
+
+        return allSlidesContent;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    case ".docx":
+      try {
+        loader = new DocxLoader(filePath);
+        docs = await loader.load();
+        allSlidesContent = docs.map((doc) => doc.pageContent);
+
+        return allSlidesContent;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    case ".txt":
+      try {
+        loader = new TextLoaderLoader(filePath);
+        docs = await loader.load();
+        allSlidesContent = docs.map((doc) => doc.pageContent);
+
+        return allSlidesContent;
+      } catch (err) {
+        throw new Error(err.message);
+      }
     default:
       throw new Error(`Unsupported file type: ${ext}`);
   }
-}
+};
 
-export {parser}
+export { parser };
